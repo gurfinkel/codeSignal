9cc7b3f5ca89fd6a7d007300fd13b7c4e64db211
Dmitry <flatorez@ya.ru>
2020-01-15 20:20:54 +0100

    added solutions

--- tournaments/twoArraysNthElement/twoArraysNthElement.js
+++ tournaments/twoArraysNthElement/twoArraysNthElement.js
@@ -1,37 +1,3 @@
 function twoArraysNthElement(array1, array2, n) {
-
-  var lowerBound = function(array, elem) {
-    var l = -1;
-    var r = array.length;
-    while ( l + 1 < r ) {
-      var mid = Math.floor((l + r) / 2);
-      if (array[mid] <= elem) {
-        l = mid;
-      }
-      else {
-        r = mid;
-      }
-    }
-    return l;
-  };
-
-  var l = -1;
-  var r = array1.length;
-
-  while (l + 1 < r) {
-    var mid = Math.floor((l + r) / 2);
-    var pos = lowerBound(array2, array1[mid]);
-
-    if (mid + pos + 1 <= n) {
-      l = mid;
-    }
-    else {
-      r = mid;
-    }
-  }
-
-  if (l > -1 && l + lowerBound(array2, array1[l]) + 1 === n) {
-    return array1[l];
-  }
-  return twoArraysNthElement(array2, array1, n);
+  return [...array1, ...array2].sort((x, y) => x - y)[n];
 }
