cee980fe3eb4afd8808c0440a3082d989f63b178
Dmitry <flatorez@ya.ru>
2020-02-18 08:05:23 +0100

    add solutions

---
+++ tournaments/isTandemRepeat/isTandemRepeat.js
@@ -0,0 +1,13 @@
+function isTandemRepeat(inputString) {
+    if (inputString.length & 1) {
+        return false;
+    }
+    
+    for (let i = 0; (inputString.length >> 1) > i; ++i) {
+        if (inputString[i] !== inputString[(inputString.length >> 1) + i]) {
+            return false;
+        }
+    }
+    
+    return true;
+}
