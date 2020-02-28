79745fc1d27f023390d730afd622dbe46e24d0a1
Dmitry Blinov <dmitry.blinov@booking.com>
2020-02-28 17:07:51 +0100

    add solutions

--- tournaments/isMAC48Address/isMAC48Address.js
+++ tournaments/isMAC48Address/isMAC48Address.js
@@ -1,19 +1,3 @@
 function isMAC48Address(inputString) {
-
-    for (var i = 0; i < inputString.length; i++) {
-        if (i % 3 === 2) {
-            if (inputString[i] !== '-') {
-            return false;
-            }
-        }
-        else {
-            var sym = inputString[i];
-            if (!('0' <= sym && sym <= '9' || 'A' <= sym && sym <= 'F')) {
-            return false;
-            }
-        }
-    }
-  
-    return inputString.length === 17;
+    return /^([0-9A-F]{2}-){5}[0-9A-F]{2}$/.test(inputString);
 }
-    
